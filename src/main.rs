bae5160a6e9b486b559dec5a26894541d79c272b
CheshirskiyCat <mail@mail.ru>
2025-03-09 10:39:37 +0300

    HW-5 ring buffer

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
+use library::{create, write, read};
+
 fn main() {
+    let mut buffer = create(3);
+    let step1 = write(&mut buffer, vec!['a','b']);
+    println!("{:?}", step1);
+    let step2 = write(&mut buffer, vec!['c','d']);
+    println!("{:?}", step2);
+    let step3 = read(&mut buffer, 1);
+    println!("{:?}", step3);
+    let step4 = write(&mut buffer, vec!['e']);
+    println!("{:?}", step4);
+    let step5 = read(&mut buffer, 2);
+    println!("{:?}", step5);
+
 }
+
